906f6fad0fcb586179b7fb32ad5c87aad3a089c8
MaxNo1969 <maxnov@mail.ru>
2024-05-29 17:06:44 +0500

    Настройка Result

--- Data/Result.cs
+++ Data/Result.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using USPC;
 
 namespace Data
 {
@@ -9,40 +10,44 @@ namespace Data
     {
         public int sensors;
         public int zones;
+        public int board;
         public List<List<double>> values;
         public virtual void addZone()
         {
+            zoneStart.Add(Program.data[board].currentOffsetFrames);
         }
         public List<int> zoneStart;
-        public Result(int _sensors)
+        public Result(int _board,int _sensors)
         {
+            board = _board;
             sensors = _sensors;
             zones = 0;
             values = new List<List<double>>();
             zoneStart = new List<int>();
+            zoneStart.Add(0);
         }
     }
 
     class CrossResult : Result
     {
-        public CrossResult(int _sensors):base(_sensors){}
-        public override void addZone()
+        public CrossResult(int _sensors):base(0,_sensors){}
+        public override void addZone(int _board)
         {
             base.addZone();
         }
     }
     class LinearResult : Result
     {
-        public LinearResult(int _sensors):base(_sensors){}
-        public override void addZone()
+        public LinearResult(int _sensors):base(0,_sensors){}
+        public override void addZone(int _board)
         {
             base.addZone();
         }
     }
     class ThickResult : Result
     {
-        public ThickResult(int _sensors) : base(_sensors) { }
-        public override void addZone()
+        public ThickResult(int _sensors) : base(1,_sensors) { }
+        public override void addZone(int _board)
         {
             base.addZone();
         }
