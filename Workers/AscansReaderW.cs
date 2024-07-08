f9040c29c348b4f4dd824959c004d45290632984
MaxNo1969 <maxnov@mail.ru>
2024-07-08 10:05:30 +0500

    Сбор данных потоками

--- Workers/AscansReaderW.cs
+++ Workers/AscansReaderW.cs
@@ -3,35 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PROTOCOL;
+using System.Threading;
 
 namespace USPC.Workers
 {
-    class AscansReader
+    class AscansReaderW
     {
-        List<ChannelReaderThread> chReaders = new List<ChannelReaderThread>();
-        public AscansReader(int _timeout)
+        List<ChannelAscansReader> chReaders = new List<ChannelAscansReader>();
+        public AscansReaderW(int _timeout)
         {
             for (int board = 0; board < Program.numBoards; board++)
             {
                 for (int channel = 0; channel < Program.channelsOnBoard[board]; channel++)
                 {
-                    chReaders.Add(new ChannelReaderThread(board, channel));
+                    chReaders.Add(new ChannelAscansReader(board, channel, _timeout));
                 }
             }
         }
         public void start()
         {
             log.add(LogRecord.LogReason.debug, "{0}: {1}: Будем запускать {2} потоков", GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, chReaders.Count);
-            foreach (ChannelReaderThread reader in chReaders)
+            foreach (ChannelAscansReader reader in chReaders)
             {
-                reader.start();
+                if(!reader.IsBusy)reader.RunWorkerAsync();
+                while (!reader.IsBusy) Thread.Sleep(10);
             }
         }
         public void stop()
         {
-            foreach (ChannelReaderThread reader in chReaders)
+            foreach (ChannelAscansReader reader in chReaders)
             {
-                reader.stop();
+                if(reader.IsBusy)reader.CancelAsync();
             }
         }
     }
---
+++ Workers/ChannelReaderThread.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using USPC.Data;
+using Data;
+using Settings;
+using PROTOCOL;
+
+namespace USPC.Workers
+{
+    class ChannelReaderThread
+    {
+        int board = 0;
+        int channel = 0;
+        Thread thread = null;
+        ThreadStart threadStart = null;
+        bool terminate = false;
+        Result result = Program.result;
+        int timeout = AppSettings.s.BoardReadTimeout;
+
+        public ChannelReaderThread(int _board, int _channel)
+        {
+            board = _board;
+            channel = _channel;
+            threadStart = new ThreadStart(ReadAscans);
+            thread = new Thread(threadStart);
+        }
+        public void start()
+        {
+            thread.Start();
+        }
+        public void stop()
+        {
+            terminate = true;
+            thread.Join();
+        }
+        void ReadAscans()
+        {
+            while (!terminate)
+            {
+                //Получим номер канала в result
+                int resultChannel = board * 4 + channel;
+                Ascan ascan = new Ascan();
+                if (Program.pcxus.readAscan(board, channel, ref ascan, timeout))
+                {
+                    double amp = ascan.G1Amp;
+                    uint tof = ascan.G1TofWt * 5;
+                    double thick = ThickConverter.TofToMm(tof);
+                    double val = (board == 0) ? thick : amp;
+                    if (result.values[result.zone] != null && result.values[result.zone][resultChannel] != null)
+                    {
+                        result.values[result.zone][resultChannel].Add(ascan);
+                        //log.add(LogRecord.LogReason.debug, "{0}: {1}: board={2} channel={3} zone={4} count={5} val={6}", 
+                        //    GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, 
+                        //    board, channel,result.zone,result.values[result.zone][resultChannel].Count,val);
+                    }
+                    else
+                    {
+                        log.add(LogRecord.LogReason.error, "{0}: {1}: {2}", GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, "Не добавлена зона или датчик");
+                    }
+                }
+                else
+                {
+                    Program.result.values[result.zone][resultChannel].Add(result.notMeasuredAscan);
+                    log.add(LogRecord.LogReason.warning, "{0}: {1}: Не удалось прочитать ascan board={2} channel={3}", GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, board, channel);
+                }
+            }
+        }
+    }
+}
